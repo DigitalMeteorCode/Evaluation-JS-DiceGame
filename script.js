98a7c64085baa042a8901dd45187dc58cfea3a67
Sarah Beaubeau <sarah.beaubeau@hotmail.fr>
2021-08-22 17:25:58 +0200

    Eval2 - Tests, corrections et cleaning du code

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-//Initialisation du jeu
+//Game initialisation
 newGame();
 
-//Déclaration des variables et constantes
-
-/*let p1 = document.getElementById("p1");
-let p2 = document.getElementById("p2");
-let globalP1 = document.getElementById("globalP1");
-let globalP2 = document.getElementById("globalP2");
-let currentP1 = document.getElementById("currentP1");
-let currentP2 = document.getElementById("currentP2");
-
-let bgColor = document.getElementById("bgColor");*/
-
-
+//Declaring const and let
 const roll = document.getElementById("roll");
 const hold = document.getElementById("hold");
 const newGameButton = document.getElementById("newGameButton");
 const closeModal = document.getElementById("modalClose");
 
-
+let modalVictory = document.getElementById("modal");
+let victoryMsg = document.getElementById("victoryMessage");
 let diceImage = document.getElementById("dice");
 let diceNumber = {
     1: "img/Dice1.png",
@@ -28,11 +18,8 @@ let diceNumber = {
     5: "img/Dice5.png" ,
     6: "img/Dice6.png"
   }
-let modalVictory = document.getElementById("modal");
-let victoryMsg = document.getElementById("victoryMessage");
 
-//Initialisation du jeu
-
+//Game initialisation function
 function newGame () {
     console.log ("New game launched");
     globalP1 = 0;
@@ -47,10 +34,8 @@ function newGame () {
     player = 1;
     playerActive();
 }
-newGameButton.addEventListener('click', newGame);
 
-
-//player active
+//Active player function
 function playerActive(){
     let p1 = document.getElementById("p1");
     let p2 = document.getElementById("p2");
@@ -70,16 +55,14 @@ function playerActive(){
         p1.style.fontWeight = 'normal';
         bgColor.setAttribute("style", "background-image:linear-gradient(to left, #EDEFF2, #EDEFF2 50%, #ffffff 50%, #ffffff);");
     }
-    else /*((player == 2)&&(window.innerWidth<1024))*/ {
+    else {
         p2.style.fontWeight = 'bold';
         p1.style.fontWeight = 'normal';
         bgColor.setAttribute("style", "background-image:linear-gradient(to top, #EDEFF2, #EDEFF2 50%, #ffffff 50%, #ffffff);");
     }
-
 }
 
-//Lancer de dé
-
+//Rolling dice function
 function rollDice(){
     diceImage.classList.add("dice_animation");
     setTimeout(function () {
@@ -91,35 +74,28 @@ function rollDice(){
     if ((player == 1)&&(diceScore != 1)) {
         currentP1 = currentP1 + diceScore;
         document.getElementById("currentP1").innerHTML = currentP1;
-        //diceImage.classList.remove("dice_animation");
     }
     else if ((player == 1)&&(diceScore == 1)) {
         currentP1 = 0;
         document.getElementById("currentP1").innerHTML = currentP1;
         player = 2;
-        //diceImage.classList.remove("dice_animation");
-        playerActive();        
+        playerActive();
     }
     else if ((player == 2)&&(diceScore != 1)) {
         currentP2 = currentP2 + diceScore;
         document.getElementById("currentP2").innerHTML = currentP2;
-        //diceImage.classList.remove("dice_animation");
     }
     else {
         currentP2 = 0;
         document.getElementById("currentP2").innerHTML = currentP2;
         player = 1;
-        //diceImage.classList.remove("dice_animation");
         playerActive();
     }
-    //diceImage.classList.remove("dice_animation");
 }
-roll.addEventListener('click', rollDice);
 
-//Hold score ATTENTION : SCORE VICTOIRE CHANGE A  : REMETTRE 100!
-
+//Holding score function 
 function holdScore(){
-    if ((player == 1)&&(globalP1+currentP1<20)){
+    if ((player == 1)&&(globalP1+currentP1<100)){
         globalP1 = globalP1 + currentP1;
         currentP1 = 0;
         document.getElementById("currentP1").innerHTML = currentP1;
@@ -127,12 +103,11 @@ function holdScore(){
         player = 2;
         playerActive();
     }
-    else if ((player == 1)&&(globalP1+currentP1>=20)){
+    else if ((player == 1)&&(globalP1+currentP1>=100)){
         modalVictory.style.display = "block";
         victoryMsg.innerHTML = "PLAYER ONE WINS";
-        //newGame();
     }
-    else if ((player == 2)&&(globalP2+currentP2<20)){
+    else if ((player == 2)&&(globalP2+currentP2<100)){
         globalP2 = globalP2 + currentP2;
         currentP2 = 0;
         document.getElementById("currentP2").innerHTML = currentP2;
@@ -140,23 +115,22 @@ function holdScore(){
         player = 1;
         playerActive();
     }
-    else if ((player == 2)&&(globalP2+currentP2>=20)){
+    else if ((player == 2)&&(globalP2+currentP2>=100)){
         modalVictory.style.display = "block";
         victoryMsg.innerHTML = "PLAYER TWO WINS";
-        //newGame();
     }
 }
-hold.addEventListener('click', holdScore);
 
-
+//Closing victory modal function
 function close(){
     victoryMsg.innerHTML = "";
     modalVictory.style.display = "none";
     console.log("close");
-    
     newGame();
 }
+
+//Buttons event listening
+newGameButton.addEventListener('click', newGame);
+roll.addEventListener('click', rollDice);
+hold.addEventListener('click', holdScore);
 closeModal.addEventListener ('click', close);
-
-
-
